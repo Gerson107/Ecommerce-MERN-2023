034f4bc0e4db76672a1f824b831ef41312f242f9
Gerson107 <danimejia1995@hotmail.com>
2023-10-31 17:15:16 -0500

    create admin product

---
+++ client/src/pages/Admin/Product.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+
+
+ const Product = () => {
+    const [products, setProducts] = useState([]);
+
+    //getall products
+    const getAllProducts = async () => {
+      try {
+        const { data } = await axios.get("/api/v1/product/get-product");
+        setProducts(data.products);
+      } catch (error) {
+        if (setProducts) {
+          toast.error(error.response)
+        } else {
+          console.log(error);
+          toast.error("Someething Went Wrong");
+        }
+       
+      }
+    };
+  
+    //lifecycle method
+    useEffect(() => {
+      getAllProducts();
+    }, []);
+    return (
+      <Layout>
+        <div className="row">
+          <div className="col-md-4">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1 className="text-center">All Products List</h1>
+            <div className="d-flex">
+              {products?.map((p) => (
+                <Link
+                  key={p._id}
+                  to={`/dashboard/admin/product/${p.slug}`}
+                  className="product-link"
+                >
+                  <div className="card m-2" style={{ width: "18rem" }}>
+                    <img
+                    style={{maxHeight:"17rem"}}
+                      src={`/api/v1/product/product-photo/${p._id}`}
+                      className="card-img-top"
+                      alt={p.name}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{p.name}</h5>
+                      <p className="card-text"></p>
+                      <p className="card-text">${p.price}</p>
+                      <p className="card-text">Stock: {p.quantity}</p>
+
+
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Layout>
+   )
+ }
+ 
+ export default Product
